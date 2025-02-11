5fabed339028b7d9ad50a21baf4448f473ea7107
wotanut <sam@sambot.dev>
2025-02-11 13:49:45 +0000

    worksheet 1

--- week1.hs
+++ week1.hs
@@ -8,23 +8,22 @@ areaOfCircle :: Float -> Float
 areaOfCircle r = pi * r^2
 
 volumeOfCylinder :: Float -> Float -> Float
-volumeOfCylinder height radius = area * height
-    where area = areaOfCircle radius
+volumeOfCylinder height radius = areaOfCircle radius * height
 
 distance :: Float -> Float -> Float -> Float -> Float
-distance x1 y1 x2 y2 = sqrt( (y2-y1)^2 + (x2-x1)^2)
+distance x1 y1 x2 y2 = sqrt ( (y2 - y1) ^ 2 + (x2 - x1)^ 2)
 
 threeDifferent :: Int -> Int -> Int -> Bool
-threeDifferent x y z = x /= y && y /= z
+threeDifferent x y z = x /= y && y /= z && z /= x
 
 divisibleBy :: Int -> Int -> Bool
 divisibleBy x y = x `mod` y == 0
 
 isEven :: Int -> Bool
-isEven x = divisibleBy x 2
+isEven x = x `divisibleBy` 2
 
 averageThree :: Int -> Int -> Int -> Float
 averageThree x y z = fromIntegral (x + y + z) / 3
 
 absolute :: Int -> Int
-absolute x = if x > 0 then x else (x * (-1))
+absolute x = if x > 0 then x else -x
